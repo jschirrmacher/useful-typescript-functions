ce794eabe7af0d7eeac2113ffd1e893120c680b5
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2023-10-24 06:15:11 +0200

    chore: remove support for time without timezone in JSON reader

--- dist/cjs/test-globals.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.setup = void 0;
-const setup = () => {
-    process.env.TZ = "Europe/Berlin";
-};
-exports.setup = setup;
-//# sourceMappingURL=test-globals.js.map
--- dist/esm/test-globals.js
+++
@@ -1,4 +0,0 @@
-export const setup = () => {
-    process.env.TZ = "Europe/Berlin";
-};
-//# sourceMappingURL=test-globals.js.map
