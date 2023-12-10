a623e62bd0309bac50ddace408b092825c8418a6
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2023-12-10 09:22:06 +0100

    fix: test assertions

--- src/Configuration.test.ts
+++ src/Configuration.test.ts
@@ -2,7 +2,6 @@ import { describe, expect, it, vi } from "vitest"
 import { Configuration } from "./Configuration.js"
 
 const content = `frontendTest: 42
-jwtSecret: my-secret
 backend:
   backendTest: 815
 `
@@ -17,21 +16,21 @@ function getMockFs() {
 describe("Configuration", () => {
   it("should return frontend configuration info", () => {
     const { frontendConfiguration } = Configuration("/conf/config.yaml", getMockFs())
-    expect(frontendConfiguration).toContain({ frontendTest: 42 })
+    expect(frontendConfiguration).toEqual({ frontendTest: 42 })
   })
 
-  it("should not include backendConfiguration in frontendCondiguration", () => {
+  it("should not include backendConfiguration in frontendConfiguration", () => {
     const { frontendConfiguration } = Configuration("/conf/config.yaml", getMockFs())
     expect(frontendConfiguration).not.toHaveProperty("backend")
   })
 
   it("should return backend configuration info", () => {
     const { backendConfiguration } = Configuration("/conf/config.yaml", getMockFs())
-    expect(backendConfiguration).toContain({ backendTest: 815 })
+    expect(backendConfiguration).toEqual({ backendTest: 815 })
   })
 
   it("should return the default configuration if the file doesn't exist", () => {
     const { frontendConfiguration } = Configuration("non-existing.yaml")
-    expect(frontendConfiguration).toContain({ isDefault: true })
+    expect(frontendConfiguration).toEqual({ isDefault: true })
   })
 })
