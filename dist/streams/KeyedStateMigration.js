6ed8f332779163e81ea27dec521ab7c03c6477b5
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2024-03-29 06:55:53 +0100

    fix: update generated files from 40be856e0f02ee42c4993d0be87b39fd8ba570c8

--- dist/streams/KeyedStateMigration.js
+++ dist/streams/KeyedStateMigration.js
@@ -1,17 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.KeyedStateMigration = void 0;
+const typeorm_1 = require("typeorm");
 class KeyedStateMigration {
     async up(queryRunner) {
-        await queryRunner.query(`CREATE TABLE "keyedstate" (
-      "id" text NOT NULL,
-      "key" text NOT NULL,
-      "state" text NOT NULL,
-      CONSTRAINT "PK_keyedstate_id_key" PRIMARY KEY ("id", "key")
-    )`);
+        await queryRunner.createTable(new typeorm_1.Table({
+            name: "keyedstate",
+            columns: [
+                { name: "id", type: "text", isNullable: false, isPrimary: true },
+                { name: "key", type: "text", isNullable: false, isPrimary: true },
+                { name: "state", type: "text", isNullable: false },
+            ],
+        }), true);
     }
     async down(queryRunner) {
-        await queryRunner.query(`DROP TABLE "keyedstate"`);
+        await queryRunner.dropTable("keyedstate");
     }
 }
 exports.KeyedStateMigration = KeyedStateMigration;
